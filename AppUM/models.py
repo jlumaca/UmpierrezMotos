9ff6f4ed0968b1ff3234377346a3626b422d5f55
Juan Lumaca <lumacajuanmanuel@gmail.com>
2024-10-21 16:39:49 -0300

    Tablas Personal, Administrativo y Mecanico

--- AppUM/models.py
+++ AppUM/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Personal(models.Model):
+    documento = models.CharField(max_length=20, unique=True)
+    nombre = models.CharField(max_length=20)
+    apellido  = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField()
+    usuario = models.CharField(max_length=10,unique=True)
+    contrasena = models.CharField(max_length=20)
+    correo = models.CharField(max_length=40,unique=True)
+    telefono = models.CharField(max_length=10,unique=True) 
+
+class Administrativo(Personal):
+    activo = models.BooleanField(default=True)
+
+class Mecanico(Personal):
+    jefe = models.BooleanField(default=False)
+    activo = models.BooleanField(default=True)
+

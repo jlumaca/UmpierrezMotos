5573385a60a2f51d728f576d1a13da2f8445b6eb
Juan Lumaca <lumacajuanmanuel@gmail.com>
2024-10-22 12:45:09 -0300

    carpeta media, modelos Moto y accesorio

--- AppUM/models.py
+++ AppUM/models.py
@@ -18,3 +18,26 @@ class Mecanico(Personal):
     jefe = models.BooleanField(default=False)
     activo = models.BooleanField(default=True)
 
+class Moto(models.Model):
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=20)
+    anio = models.IntegerField()
+    motor = models.IntegerField()
+    estado = models.CharField(max_length=10)
+    kilometros = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    color = models.CharField(max_length=20)
+    matricula = models.CharField(max_length=20, unique=True)
+    num_motor = models.CharField(max_length=20, unique=True)
+    num_chasis = models.CharField(max_length=20, unique=True)
+    foto = models.ImageField(null=True, blank=True, upload_to="motos/")
+    pertenece_tienda = models.BooleanField()
+    pertenece_taller = models.BooleanField()
+
+class accesorio(models.Model):
+    tipo = models.CharField(max_length=20)
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=20)
+    foto = models.ImageField(null=True, blank=True, upload_to="accesorios/")
+    activo = models.BooleanField()
+

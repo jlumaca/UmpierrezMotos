b265d0cc506aeca7d2d9cbba38cc10cf75992fcc
Juan Lumaca <lumacajuanmanuel@gmail.com>
2025-03-12 17:39:19 -0300

    hora de envio de notificaciones

--- AppUM/scheduler.py
+++ AppUM/scheduler.py
@@ -7,7 +7,7 @@ def iniciar_scheduler():
     
     # Programar la función para que se ejecute todos los días a las 9 AM
     scheduler.add_job(mi_funcion_diaria, 
-                      trigger=CronTrigger(hour=10, minute=20), 
+                      trigger=CronTrigger(hour=6, minute=00), 
                       id="mi_funcion_diaria", 
                       replace_existing=True)
     
